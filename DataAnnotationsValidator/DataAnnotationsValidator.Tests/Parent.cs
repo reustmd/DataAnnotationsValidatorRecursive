63665506473ded60143ce761bc719a99d0311c21
Mike Reust <reust@betterment.com>
2017-07-02 16:36:39 -0400

    standardizing formatting with lineendings and spaces

--- DataAnnotationsValidator/DataAnnotationsValidator.Tests/Parent.cs
+++ DataAnnotationsValidator/DataAnnotationsValidator.Tests/Parent.cs
@@ -3,28 +3,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DataAnnotationsValidator.Tests
 {
-	public class Parent : IValidatableObject
-	{
-		[Required(ErrorMessage = "Parent PropertyA is required")]
-		[Range(0, 10, ErrorMessage = "Parent PropertyA not within range")]
-		public int? PropertyA { get; set; }
+    public class Parent : IValidatableObject
+    {
+        [Required(ErrorMessage = "Parent PropertyA is required")]
+        [Range(0, 10, ErrorMessage = "Parent PropertyA not within range")]
+        public int? PropertyA { get; set; }
 
-		[Required(ErrorMessage = "Parent PropertyB is required")]
-		[Range(0, 10, ErrorMessage = "Parent PropertyB not within range")]
-		public int? PropertyB { get; set; }
+        [Required(ErrorMessage = "Parent PropertyB is required")]
+        [Range(0, 10, ErrorMessage = "Parent PropertyB not within range")]
+        public int? PropertyB { get; set; }
 
-		public Child Child { get; set; }
+        public Child Child { get; set; }
 
-		[SkipRecursiveValidation]
-		public Child SkippedChild { get; set; }
+        [SkipRecursiveValidation]
+        public Child SkippedChild { get; set; }
 
-		[SaveValidationContext]
-		public bool HasNoRealValidation { get; set; }
+        [SaveValidationContext]
+        public bool HasNoRealValidation { get; set; }
 
-		IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-		{
-			if (PropertyA.HasValue && PropertyB.HasValue && (PropertyA + PropertyB > 10))
-				yield return new ValidationResult("Parent PropertyA and PropertyB cannot add up to more than 10");
-		}
-	}
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        {
+            if (PropertyA.HasValue && PropertyB.HasValue && (PropertyA + PropertyB > 10))
+                yield return new ValidationResult("Parent PropertyA and PropertyB cannot add up to more than 10");
+        }
+    }
 }
--- DataAnnotationsValidator/DataAnnotationsValidator/DataAnnotationsValidator.cs
+++ DataAnnotationsValidator/DataAnnotationsValidator/DataAnnotationsValidator.cs
@@ -6,62 +6,62 @@ using System.Reflection;
 
 namespace DataAnnotationsValidator
 {
-	public class DataAnnotationsValidator : IDataAnnotationsValidator
-	{
-		public bool TryValidateObject(object obj, ICollection<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
-		{
-			return Validator.TryValidateObject(obj, new ValidationContext(obj, null, validationContextItems), results, true);
-		}
+    public class DataAnnotationsValidator : IDataAnnotationsValidator
+    {
+        public bool TryValidateObject(object obj, ICollection<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
+        {
+            return Validator.TryValidateObject(obj, new ValidationContext(obj, null, validationContextItems), results, true);
+        }
 
-		public bool TryValidateObjectRecursive<T>(T obj, List<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
-		{
-			bool result = TryValidateObject(obj, results, validationContextItems);
+        public bool TryValidateObjectRecursive<T>(T obj, List<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
+        {
+            bool result = TryValidateObject(obj, results, validationContextItems);
 
-            var properties = obj.GetType().GetProperties().Where(prop => prop.CanRead 
-                && !prop.GetCustomAttributes(typeof(SkipRecursiveValidation), false).Any() 
+            var properties = obj.GetType().GetProperties().Where(prop => prop.CanRead
+                && !prop.GetCustomAttributes(typeof(SkipRecursiveValidation), false).Any()
                 && prop.GetIndexParameters().Length == 0).ToList();
 
-			foreach (var property in properties)
-			{
-				if (property.PropertyType == typeof(string) || property.PropertyType.IsValueType) continue;
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == typeof(string) || property.PropertyType.IsValueType) continue;
 
-				var value = obj.GetPropertyValue(property.Name);
+                var value = obj.GetPropertyValue(property.Name);
 
-				if (value == null) continue;
+                if (value == null) continue;
 
-				var asEnumerable = value as IEnumerable;
-				if (asEnumerable != null)
-				{
-					foreach (var enumObj in asEnumerable)
-					{
-						var nestedResults = new List<ValidationResult>();
-						if (!TryValidateObjectRecursive(enumObj, nestedResults, validationContextItems))
-						{
-							result = false;
-							foreach (var validationResult in nestedResults)
-							{
-								PropertyInfo property1 = property;
-								results.Add(new ValidationResult(validationResult.ErrorMessage, validationResult.MemberNames.Select(x => property1.Name + '.' + x)));
-							}
-						};
-					}
-				}
-				else
-				{
-					var nestedResults = new List<ValidationResult>();
-					if (!TryValidateObjectRecursive(value, nestedResults, validationContextItems))
-					{
-						result = false;
-						foreach (var validationResult in nestedResults)
-						{
-							PropertyInfo property1 = property;
-							results.Add(new ValidationResult(validationResult.ErrorMessage, validationResult.MemberNames.Select(x => property1.Name + '.' + x)));
-						}
-					};
-				}
-			}
+                var asEnumerable = value as IEnumerable;
+                if (asEnumerable != null)
+                {
+                    foreach (var enumObj in asEnumerable)
+                    {
+                        var nestedResults = new List<ValidationResult>();
+                        if (!TryValidateObjectRecursive(enumObj, nestedResults, validationContextItems))
+                        {
+                            result = false;
+                            foreach (var validationResult in nestedResults)
+                            {
+                                PropertyInfo property1 = property;
+                                results.Add(new ValidationResult(validationResult.ErrorMessage, validationResult.MemberNames.Select(x => property1.Name + '.' + x)));
+                            }
+                        };
+                    }
+                }
+                else
+                {
+                    var nestedResults = new List<ValidationResult>();
+                    if (!TryValidateObjectRecursive(value, nestedResults, validationContextItems))
+                    {
+                        result = false;
+                        foreach (var validationResult in nestedResults)
+                        {
+                            PropertyInfo property1 = property;
+                            results.Add(new ValidationResult(validationResult.ErrorMessage, validationResult.MemberNames.Select(x => property1.Name + '.' + x)));
+                        }
+                    };
+                }
+            }
 
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 }
