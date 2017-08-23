c8ce8f866e521ac680ae7f8dadac3ec5fc88c709
Shimmy <weitzhandler@users.noreply.github.com>
2017-08-23 10:36:19 +0300

    It should be a static class

--- DataAnnotationsValidator/DataAnnotationsValidator/DataAnnotationsValidator.cs
+++ DataAnnotationsValidator/DataAnnotationsValidator/DataAnnotationsValidator.cs
@@ -6,19 +6,19 @@ using System.Reflection;
 
 namespace DataAnnotationsValidator
 {
-    public class DataAnnotationsValidator : IDataAnnotationsValidator
+    public static class DataAnnotationsValidator : IDataAnnotationsValidator
     {
-        public bool TryValidateObject(object obj, ICollection<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
+        public static bool TryValidateObject(object obj, ICollection<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
         {
             return Validator.TryValidateObject(obj, new ValidationContext(obj, null, validationContextItems), results, true);
         }
 
-        public bool TryValidateObjectRecursive<T>(T obj, List<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
+        public static bool TryValidateObjectRecursive<T>(T obj, List<ValidationResult> results, IDictionary<object, object> validationContextItems = null)
         {
             return TryValidateObjectRecursive(obj, results, new HashSet<object>(), validationContextItems);
         }
 
-        private bool TryValidateObjectRecursive<T>(T obj, List<ValidationResult> results, ISet<object> validatedObjects, IDictionary<object, object> validationContextItems = null)
+        private static bool TryValidateObjectRecursive<T>(T obj, List<ValidationResult> results, ISet<object> validatedObjects, IDictionary<object, object> validationContextItems = null)
         {
             //short-circuit to avoid infinit loops on cyclical object graphs
             if (validatedObjects.Contains(obj))
@@ -30,7 +30,7 @@ namespace DataAnnotationsValidator
             bool result = TryValidateObject(obj, results, validationContextItems);
 
             var properties = obj.GetType().GetProperties().Where(prop => prop.CanRead
-                && !prop.GetCustomAttributes(typeof(SkipRecursiveValidation), false).Any()
+                && !prop.GetCustomAttributes(typeof(SkipRecursiveValidationAttribute), false).Any()
                 && prop.GetIndexParameters().Length == 0).ToList();
 
             foreach (var property in properties)
